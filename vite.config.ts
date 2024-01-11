eb6d59a92c0cc58aff9da15a42af00c243cafd93
김준극 <wise7034@gmail.com>
2024-01-11 11:22:17 +0900

    feat: test 환경 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: { port: 3000, open: true },
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./vitest.setup.ts",
+    coverage: {
+      clean: true,
+      provider: "v8",
+    },
+  },
+});
